5a1af0172ed68182d054dc05b972e190178d3d3f
BaiJiFeiLong <baijifeilong@gmail.com>
2017-09-23 16:56:22 +0800

    1. Application rename to Antea Free Chat 2. Update main page layout 3. Add test page

--- app/src/main/java/io/github/baijifeilong/antea/MainActivity.java
+++ app/src/main/java/io/github/baijifeilong/antea/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, DebugActivity.class));
             }
         });
+        findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
+
+        TextView appDescriptionTextView = findViewById(R.id.app_description_text_view);
+        appDescriptionTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        appDescriptionTextView.setText(Html.fromHtml(getString(R.string.app_description)));
     }
 }
